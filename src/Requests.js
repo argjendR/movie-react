7cbf67746ae2f345b96aa747749620893e95182f
argjendR <gentxh@gmail.com>
2022-11-21 14:04:31 +0100

    after first commit

--- src/Requests.js
+++ src/Requests.js
@@ -1,16 +1,21 @@
 
 
-const request = {
-    requestNowplaying:'https://api.themoviedb.org/3/movie/now_playing?api_key=867de60de22963b877bb3e147a8c5b3a&language=en-US&page=1',
-    requestPopular: 'https://api.themoviedb.org/3/movie/popular?api_key=867de60de22963b877bb3e147a8c5b3a&language=en-US&page=1',
-    // requestToprated: 'https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1',
-    // requestUpcoming: 'https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1',
+const key = '867de60de22963b877bb3e147a8c5b3a'
 
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestPeople: ` https://api.themoviedb.org/3/person/{person_id}/images?api_key=${key}`
+    // requestUpComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    // requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    // requestLatest: `https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`,
+    // requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`
 
+   
+    // requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    // requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    // requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1`,
+    // requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
 
-
-
-
-};
-
-export default Request
+  export default requests
